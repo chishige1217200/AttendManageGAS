f39354f097b70dae9602ef3750ff19f94c01e1fe
chishige1217200 <daichicchi01@gmail.com>
2021-12-24 15:09:42 +0900

    Add: filter, 最大横幅の変数

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var max_width = 20; // Configシートの横項目読み取り最大数
+
 function setup() { // 初期設定
   const ss = SpreadsheetApp.getActiveSpreadsheet();
   let configSheet = ss.getSheetByName('Config');
@@ -17,9 +19,13 @@ function setup() { // 初期設定
 }
 
 function setupConfig(sheet) { // Configシートの自動作成
-  const data1 = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]];
-  const data2 = [['実施回'], ['時間帯'], ['場所'], ['班数'], ['集計区別']];
-  sheet.getRange(2, 3, 1, 15).setValues(data1);
+  let data1 = [];
+  let in_data1 = [];
+  for (let i = 0; i < max_width; i++)
+    in_data1.push(i + 1);
+  data1.push(in_data1);
+  const data2 = [['実施回'], ['時間帯'], ['場所'], ['班数'], ['統計区別']];
+  sheet.getRange(2, 3, 1, max_width).setValues(data1);
   sheet.getRange(3, 2, 5, 1).setValues(data2);
 }
 
@@ -38,14 +44,14 @@ function createBase() { // Baseシートの自動作成
     baseSheet = ss.getSheetByName('Base');
   }
   let rowNum = 3;
-  let data1 = configSheet.getRange(rowNum++, 3, 1, 15).getValues();
-  let data2 = configSheet.getRange(rowNum++, 3, 1, 15).getValues();
-  let data3 = configSheet.getRange(rowNum++, 3, 1, 15).getValues();
-  let data4 = configSheet.getRange(rowNum++, 3, 1, 15).getValues();
-  let data5 = configSheet.getRange(rowNum++, 3, 1, 15).getValues();
-  console.log(data1);
-}
-
-function delNullValue(data) { // 配列の末尾要素がNULLなら除去
-  return data;
+  let data1 = configSheet.getRange(rowNum++, 3, 1, max_width).getValues();
+  let data2 = configSheet.getRange(rowNum++, 3, 1, max_width).getValues();
+  let data3 = configSheet.getRange(rowNum++, 3, 1, max_width).getValues();
+  let data4 = configSheet.getRange(rowNum++, 3, 1, max_width).getValues();
+  let data5 = configSheet.getRange(rowNum++, 3, 1, max_width).getValues();
+  part = data1[0].filter(word => word != '');
+  section = data2[0].filter(word => word != '');
+  place = data3[0].filter(word => word != '');
+  group = data4[0].filter(word => word != '');
+  statisticOption = data5[0].filter(word => word != '');
 }
