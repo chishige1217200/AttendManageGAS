740bb5bbf53e9beec5fe75788d8d24fafa5fed80
chishige1217200 <daichicchi01@gmail.com>
2022-01-09 18:09:58 +0900

    Add: 表1つ目生成処理

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ function setupConfig(sheet) { // Configシートの自動作成
   sheet.getRange(2, 3, 1, max_width).setValues(data1);
   sheet.getRange(3, 2, data2.length, 1).setValues(data2);
 
-  console.log('Configを記入してください．')
+  console.log('Configを記入してください．');
 }
 
 function createBase() { // Baseシートの自動作成
@@ -58,20 +58,73 @@ function createBase() { // Baseシートの自動作成
   let data6 = configSheet.getRange(rowNum++, 3, 1, max_width).getValues();
   let data7 = configSheet.getRange(rowNum++, 3, 1, max_width).getValues();
   let data8 = configSheet.getRange(rowNum++, 3, 1, max_width).getValues();
-  let part = data1[0].filter(word => word != ''); // 実施回
-  let section = data2[0].filter(word => word != ''); // 曜日時間帯
-  let place = data3[0].filter(word => word != ''); // 実施場所
-  let group = data4[0].filter(word => word != ''); // 班数
+  let part = data1[0].filter(word => word != ''); // 実施回（ex: 1st）
+  if (part.length <= 0) console.error('実施回は1回以上ある必要があります．');
+  let section = data2[0].filter(word => word != ''); // 曜日時間帯（ex: 月曜前半）
+  if (section.length <= 0) console.error('時間帯は1つ以上ある必要があります．');
+  let place = data3[0].filter(word => word != ''); // 実施場所（ex: A教室）
+  if (place.length <= 0) console.error('実施場所は1箇所以上ある必要があります．');
+  let group = data4[0].filter(word => word != ''); // 班数（ex: 3）
   let groupCount = []; // 教室ごとの班数をカウント
-  for (let i = 0; i < group.length; i++)
+  for (let i = 0; i < group.length; i++) {
     groupCount.push(parseInt(group[i], 10)); // 10進数でパース
+    if (groupCount[i] <= 0) console.error('班数には自然数を指定してください．');
+  }
+
+  // 出席周りは例外処理がないため，入力に注意
   let statisticOption = data5[0].filter(word => word != ''); // 集計区分要素
   let attends = data6[0].filter(word => word != ''); // 出席と記録する要素
   let absents = data7[0].filter(word => word != ''); // 欠席とする要素
   let unattends = data8[0].filter(word => word != ''); // 未処理とする要素
 
-  //console.log(groupCount);
-
   // Baseの作成
-  var totalStartColNum = 5;
+  let statisticLines = []; //各実施時間帯の集計行をマーク
+  let halfSectionCount = Math.ceil(section.length / 2); // 開始行の推定用（切り上げ）
+  let totalStartRowNum = 4 + halfSectionCount; // 1番目の表の開始行
+  let tableRowCount = 0; // 表の行数カウント
+
+  //for (let i = 0; i < section.length; i++) { // 全部の表のループ
+  let placeStatisticLines = []; // 実施場所の集計行をマーク
+  let placeStaticFormula = '=';
+
+  let firstLineArray = [section[0]];
+  for (let l = 0; l < statisticOption.length; l++)
+    firstLineArray.push(statisticOption[l]);
+  firstLineArray.push('総数');
+  firstLineArray = [firstLineArray];
+  //console.log(firstLineArray);
+  baseSheet.getRange(totalStartRowNum, 2, 1, firstLineArray[0].length).setValues(firstLineArray);
+  tableRowCount++;
+
+  for (let j = 0; j < place.length; j++) { // 実施場所毎のループ（1つの表）
+    // ここでセルに色を付けたい
+    baseSheet.getRange(totalStartRowNum + tableRowCount, 3, groupCount[j], statisticOption.length).setBackground('aqua');
+    for (let k = 0; k < groupCount[j]; k++) { // 実施場所入力行生成部
+      baseSheet.getRange(totalStartRowNum + tableRowCount, 2, 1, 1).setValue(k + 1 + '班');
+      baseSheet.getRange(totalStartRowNum + tableRowCount, 3 + statisticOption.length, 1, 1).setFormulaR1C1('=SUM(RC[' + (-statisticOption.length) + '],RC[-1])');
+      tableRowCount++;
+    }
+    baseSheet.getRange(totalStartRowNum + tableRowCount, 2, 1, 1).setValue(place[j] + '合計'); // 実施場所毎合計部
+    baseSheet.getRange(totalStartRowNum + tableRowCount, 3, 1, statisticOption.length).setFormulaR1C1('=SUM(R[' + (-groupCount[j]) + ']C,R[-1]C)');
+    baseSheet.getRange(totalStartRowNum + tableRowCount, 3 + statisticOption.length, 1, 1).setFormulaR1C1('=SUM(RC[' + (-statisticOption.length) + '],RC[-1])');
+    placeStatisticLines.push(tableRowCount++);
+  }
+
+  baseSheet.getRange(totalStartRowNum + tableRowCount, 2, 1, 1).setValue('合計').setFontColor('red');
+  for (let j = 0; j < placeStatisticLines.length; j++) {
+    placeStaticFormula += 'R' + placeStatisticLines[j] + 'C';
+    if (j + 1 !== placeStatisticLines.length) placeStaticFormula += '+';
+  }
+  baseSheet.getRange(totalStartRowNum + tableRowCount, 3, 1, statisticOption.length).setFormulaR1C1(placeStaticFormula); // 要素ごとの最終合計
+  baseSheet.getRange(totalStartRowNum + tableRowCount, 3 + statisticOption.length, 1, 1).setFormulaR1C1('=SUM(RC[' + (-statisticOption.length) + '],RC[-1])');
+
+  /*let groupCountSum = groupCount.length;
+  for (let j = 0; j < groupCount.length; j++)
+    groupCountSum += groupCount[i];
+  baseSheet.getRange(totalStartRowNum, 2, groupCountSum + 2, statisticOption.length + 2).setBorder(true, true, true, true, true, true);*/
+
+  statisticLines.push(tableRowCount + totalStartRowNum);
+  console.log(statisticLines);
+  totalStartRowNum += tableRowCount + 1;
 }
+//}
